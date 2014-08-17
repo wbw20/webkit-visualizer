b97e0caf4f4e42274dd7ea73cb192edd8d8b7e0b
William B. Wettersten <wbw20@case.edu>
2014-08-17 09:31:11 -0700

    this is the three js way

--- src/js/views/streamsView.js
+++ src/js/views/streamsView.js
@@ -6,13 +6,6 @@ App.StreamsView = Ember.View.extend({
   renderer: null,
   accelerometerBinding: 'controller.stream.accelerometer',
 
-  accelerometerObserver: function() {
-    this.set('mesh.rotation.x', this.get('accelerometer.x') * 90);
-    this.set('mesh.rotation.y', this.get('accelerometer.y') * 90);
-    this.set('mesh.rotation.z', this.get('accelerometer.z') * 90);
-    this.get('renderer').render(this.get('scene'), this.get('camera'));
-  }.observes('accelerometer'),
-
   didInsertElement: function() {
     this.set('scene', new THREE.Scene());
     this.set('camera', new THREE.PerspectiveCamera( 75, 1, 1, 10000 ));
@@ -35,6 +28,17 @@ App.StreamsView = Ember.View.extend({
   },
 
   animate: function() {
-    this.get('renderer').render(this.get('scene'), this.get('camera'));
+    var self = this;
+    var render = function () {
+      requestAnimationFrame(render);
+
+      self.set('mesh.rotation.x', self.get('accelerometer.x'));
+      self.set('mesh.rotation.y', self.get('accelerometer.y'));
+      self.set('mesh.rotation.z', self.get('accelerometer.z'));
+
+      self.get('renderer').render(self.get('scene'), self.get('camera'));
+    };
+
+    render();
   }
 });
