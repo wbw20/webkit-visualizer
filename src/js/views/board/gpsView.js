6413df52766d7fb904c47e724818184c5045fee1
William B. Wettersten <wbw20@case.edu>
2014-08-30 15:46:26 -0700

    basic mapbox

---
+++ src/js/views/board/gpsView.js
@@ -0,0 +1,7 @@
+App.BoardGpsView = Ember.View.extend({
+  didInsertElement: function() {
+    L.mapbox.accessToken = 'pk.eyJ1Ijoid3dldHRlcnN0ZW4iLCJhIjoiTWF1bTM3USJ9.cA9CFVcpcM1dN9wo1PUtPQ';
+    var map = L.mapbox.map('map', 'wwettersten.jcdhi36o')
+      .setView([40, -74.50], 9);
+    }
+});
