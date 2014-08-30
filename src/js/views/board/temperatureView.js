74bb89f10b0be2adc15f961887cc8e7e355e1fc3
William B. Wettersten <wbw20@case.edu>
2014-08-29 23:08:15 -0700

    logging temperature data

--- src/js/views/board/temperatureView.js
+++ src/js/views/board/temperatureView.js
@@ -1,16 +1,31 @@
 App.BoardTemperatureView = Ember.View.extend({
-  temperature: function() {
-    return this.get('controller.data.temperature');
-  }.property('controller.data'),
+  chart: null,
+  data: ['temperature'],
+  temperatureBinding: 'controller.data.temperature',
+
+  pushData: function(obj, key, value) {
+    if (!this.get('chart')) { return; }
+
+    this.get('data').push(this.get('temperature'));
+
+    this.get('chart').load({
+      columns: [
+        this.get('data').valueOf()
+      ]
+    });
+  }.observes('controller.data'),
 
   didInsertElement: function() {
-    $('.chart').append(c3.generate({
+    this.get('data').push(this.get('temperature'));
+
+    this.set('chart', c3.generate({
       data: {
         columns: [
-          ['data1', 100, 200, 150, 300, 200],
-          ['data2', 400, 500, 250, 700, 300],
+          this.get('data').valueOf()
         ]
       }
-    }).element);
+    }));
+
+    $('.chart').append(this.get('chart').element);
   }
 });
