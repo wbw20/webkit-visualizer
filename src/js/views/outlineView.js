e41514d001b78e5b5e35f2255a2d79ed94ecf54b
William B. Wettersten <wbw20@case.edu>
2014-08-31 20:16:20 -0700

    better svg hover

--- src/js/views/outlineView.js
+++ src/js/views/outlineView.js
@@ -1,7 +1,6 @@
 App.OutlineView = Ember.View.extend({
   templateName: 'outline',
   tagName: 'svg',
-  fill: 'none',
   viewBox: '200 0 5 200',
-  attributeBindings: ['fill', 'viewBox']
+  attributeBindings: ['viewBox'],
 });
