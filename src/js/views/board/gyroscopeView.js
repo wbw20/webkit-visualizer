645589e2c49ea78e19e5b15f3c086f895b591f2c
William B. Wettersten <wbw20@case.edu>
2014-09-07 21:31:07 -0700

    some sort of a background

--- src/js/views/board/gyroscopeView.js
+++ src/js/views/board/gyroscopeView.js
@@ -12,25 +12,29 @@ App.BoardGyroscopeView = Ember.View.extend({
     var loader = new THREE.ColladaLoader();
     loader.load('assets/models/apollo.dae', function (result) {
       self.set('figure', result.scene.children[0].clone());
-      self.set('camera', new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000 ));
-      self.get('camera').position.set( 0, 0, 150 );
+      // self.set('camera', new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000 ));
+      // self.get('camera').position.set( 0, 0, 150 );
+
+      self.set('camera', new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 5000 ));
+      self.get('camera').position.set( 0, 0, 250 );
 
       self.set('scene', new THREE.Scene());
+      self.background();
       self.get('scene').add(self.get('figure'));
 
-      self.set('renderer', new THREE.CanvasRenderer());
+      self.set('renderer', new THREE.WebGLRenderer( { antialias: true } ));
       self.get('renderer').setSize(window.innerWidth, window.innerHeight);
 
       $('.gyroscope').append(self.get('renderer.domElement'));
-      self.lights();
       self.animate();
     });
   },
 
-  lights: function() {
+  background: function() {
     var dirLight = new THREE.DirectionalLight( 0xffffff, 1 ),
         hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
 
+    /* lights */
     hemiLight.color.setHSL( 0.6, 1, 0.6 );
     hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
     hemiLight.position.set( 0, 500, 0 );
@@ -57,6 +61,40 @@ App.BoardGyroscopeView = Ember.View.extend({
 
     this.get('scene').add(hemiLight);
     this.get('scene').add(dirLight);
+
+    /* ground */
+    var groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
+    var groundMat = new THREE.MeshPhongMaterial( { ambient: 0xffffff, color: 0xffffff, specular: 0x050505 } );
+    groundMat.color.setHSL( 0.095, 1, 0.75 );
+
+    var ground = new THREE.Mesh( groundGeo, groundMat );
+    ground.rotation.x = -Math.PI/2;
+    ground.position.y = -33;
+    this.get('scene').add(ground);
+
+    ground.receiveShadow = true;
+
+    /* sky */
+    var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+    var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+    var uniforms = {
+      topColor:    { type: "c", value: new THREE.Color( 0x0077ff ) },
+      bottomColor: { type: "c", value: new THREE.Color( 0xffffff ) },
+      offset:    { type: "f", value: 33 },
+      exponent:  { type: "f", value: 0.6 }
+    }
+    uniforms.topColor.value.copy( hemiLight.color );
+
+    /* fog */
+    this.get('scene').fog = new THREE.Fog( 0xffffff, 1, 5000 );
+    this.get('scene').fog.color.setHSL( 0.6, 0, 1 );
+    this.get('scene').fog.color.copy( uniforms.bottomColor.value );
+
+    var skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+    var skyMat = new THREE.ShaderMaterial( { vertexShader: vertexShader, fragmentShader: fragmentShader, uniforms: uniforms, side: THREE.BackSide } );
+
+    var sky = new THREE.Mesh( skyGeo, skyMat );
+    this.get('scene').add(sky);
   },
 
   animate: function() {
