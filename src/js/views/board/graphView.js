49446d83c10c7395c835a88fae3ffc80bc8763f4
William B. Wettersten <wbw20@case.edu>
2014-09-12 16:55:46 -0700

    more emberish

--- src/js/views/board/graphView.js
+++ src/js/views/board/graphView.js
@@ -1,91 +1,90 @@
 App.BoardGraphView = Ember.View.extend({
+  tau: 6.283185307179586,
+
   didInsertElement: function() {
     var self = this;
 
-    $(document).ready(function() {
-      var tau = 6.283185307179586;
+    ThreeBox.preload([
+      'assets/shaders/snippets.glsl.html',
+    ], function () {
+      var el = document.getElementById('graph');
+      var mathbox = window.mathbox = mathBox(el, {
+        alpha:          true,
+        cameraControls: true,
+        cursor:         true,
+        controlClass:   ThreeBox.OrbitControls,
+        elementResize:  false,
+        fullscreen:     true,
+        screenshot:     true,
+        stats:          false,
+        scale:          1,
+      }).start();
 
-      ThreeBox.preload([
-        'assets/shaders/snippets.glsl.html',
-      ], function () {
-        var el = document.getElementById('graph');
-        var mathbox = window.mathbox = mathBox(el, {
-          alpha:          true,
-          cameraControls: true,
-          cursor:         true,
-          controlClass:   ThreeBox.OrbitControls,
-          elementResize:  false,
-          fullscreen:     true,
-          screenshot:     true,
-          stats:          false,
-          scale:          1,
-        }).start();
+      mathbox.world().tRenderer().setClearColorHex(0x000000, 0);
 
-        mathbox.world().tRenderer().setClearColorHex(0x000000, 0);
+      // Viewport camera/setup
+      mathbox.viewport({
+        type: 'cartesian',
+        range: [[-1, 1], [-1, 1], [-1, 1]],
+        scale: [2, 2, 2],
+      })
+      .camera({
+        orbit: 10,
+        phi: self.get('tau')/6,
+        theta: 0.3,
+      })
+      .transition(300)
 
-        // Viewport camera/setup
-        mathbox.viewport({
-          type: 'cartesian',
-          range: [[-1, 1], [-1, 1], [-1, 1]],
-          scale: [2, 2, 2],
-        })
-        .camera({
-          orbit: 10,
-          phi: tau/6,
-          theta: 0.3,
-        })
-        .transition(300)
+      // Axes
+      .axis({
+        id: 'a',
+        axis: 0,
+        color: 0xa0a0a0,
+        lineWidth: 2,
+        size: .05,
+        zero: false
+      })
+      .axis({
+        id: 'b',
+        axis: 1,
+        color: 0xa0a0a0,
+        lineWidth: 2,
+        size: .05,
+        zero: false
+      })
+      .axis({
+        id: 'c',
+        axis: 2,
+        color: 0xa0a0a0,
+        lineWidth: 2,
+        size: .05,
+        zero: false
+      })
 
-        // Axes
-        .axis({
-          id: 'a',
-          axis: 0,
-          color: 0xa0a0a0,
-          lineWidth: 2,
-          size: .05,
-          zero: false
-        })
-        .axis({
-          id: 'b',
-          axis: 1,
-          color: 0xa0a0a0,
-          lineWidth: 2,
-          size: .05,
-          zero: false
-        })
-        .axis({
-          id: 'c',
-          axis: 2,
-          color: 0xa0a0a0,
-          lineWidth: 2,
-          size: .05,
-          zero: false
-        })
+      // Grid
+      .grid({
+        id: 'g',
+        axis: [0, 2],
+        color: 0xc0c0c0,
+        tickScale: [2, 2],
+        ticks: [ 2, 2],
+        lineWidth: 1
+      })
 
-        // Grid
-        .grid({
-          id: 'g',
-          axis: [0, 2],
-          color: 0xc0c0c0,
-          tickScale: [2, 2],
-          ticks: [ 2, 2],
-          lineWidth: 1
-        })
-
-        mathbox.vector({
-          n: 2,
-          data: [[0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 1, .5]],
-        });
-
-        setTimeout(function() {
-          self.resetDomain([-1, 5], [-1, 1], [-1, 1]);
-        }, 3000);
-
-        setTimeout(function() {
-          self.resetDomain([-1, 5], [-1, 1], [-10, 10]);
-        }, 5000);
+      mathbox.vector({
+        n: 2,
+        data: [[0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 1, .5]],
       });
+
+      setTimeout(function() {
+        self.resetDomain([-1, 5], [-1, 1], [-1, 1]);
+      }, 3000);
+
+      setTimeout(function() {
+        self.resetDomain([-1, 5], [-1, 1], [-10, 10]);
+      }, 5000);
     });
+
   },
 
   /*
