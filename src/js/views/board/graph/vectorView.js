777ba2a5dc8843089f6e90dc8dc3a0ab593b7046
William B. Wettersten <wbw20@case.edu>
2014-09-13 14:01:59 -0700

    switch to ember views

---
+++ src/js/views/board/graph/vectorView.js
@@ -0,0 +1,10 @@
+App.VectorView = Ember.View.extend({
+  vectorBinding: null,
+  classNames: ['vector'],
+  draggable: 'true',
+  attributeBindings: ['draggable'],
+
+  dragStart: function(event) {
+    debugger
+  }
+});
